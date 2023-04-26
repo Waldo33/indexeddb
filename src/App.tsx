bd9d7e3d1a3a0e06da3e36d6d083365198d351ef
waldo <evgenijtser@gmail.com>
2023-04-26 20:45:04 +0300

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Dexie from 'dexie';
+import "./App.css"
+import {Product} from "./types/types";
+import ProductCard from "./components/ProductCard";
+
+const db = new Dexie('FakeStore');
+db.version(1).stores({
+    products: 'id',
+});
 
 function App() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const getProducts = async () => {
+        try {
+            const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
+            const products = response.data;
+
+            // Проверяем, есть ли данные уже в IndexedDB
+            const indexedDBProducts = await db.table('products').toArray();
+
+            // Если данные отсутствуют в IndexedDB, то записываем их туда.
+            if (indexedDBProducts.length === 0) {
+                await db.table('products').bulkPut(products);
+                console.log('Данные успешно записаны в IndexedDB');
+                setProducts(products);
+            } else {
+                // Если данные уже есть в IndexedDB, то сравниваем их с данными с сервера.
+                const isSameData = JSON.stringify(indexedDBProducts) === JSON.stringify(products);
+
+                if (!isSameData) {
+                    await db.table('products').clear();
+                    await db.table('products').bulkPut(products);
+                    console.log('Данные успешно обновлены в IndexedDB');
+                }
+                setProducts(indexedDBProducts);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {products.map((product) => (
+            <ProductCard
+                key={product.id}
+                product={product}
+            />
+        ))}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// if ("serviceWorker" in navigator) {
+//     window.addEventListener("load", () => {
+//         navigator.serviceWorker.register("./service-worker.js").then(
+//             (registration) => {
+//                 console.log("Service Worker registered with scope: ", registration.scope);
+//             },
+//             (err) => {
+//                 console.error("Service Worker registration failed: ", err);
+//             }
+//         );
+//     });
+// }
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register({});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
